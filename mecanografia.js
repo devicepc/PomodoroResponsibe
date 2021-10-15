bed2b9258a92515c2b1b3cea0af7886362423854
devicepc <69884110+devicepc@users.noreply.github.com>
2021-10-14 23:34:43 -0300

    Add files via upload

---
+++ mecanografia.js
@@ -0,0 +1,14 @@
+//<![CDATA[
+    let writing=e=>{let t=e.split(""),n=0,i=setInterval(function(){document.getElementById("maquinaescribir").innerHTML+=t[n],++n===t.length&&clearInterval(i)},200)};
+    let writing2=e=>{let t=e.split(""),n=0,i=setInterval(function(){document.getElementById("maquinaescribir2").innerHTML+=t[n],++n===t.length&&clearInterval(i)},200)};
+	let writing3=e=>{let t=e.split(""),n=0,i=setInterval(function(){document.getElementById("maquinaescribir3").innerHTML+=t[n],++n===t.length&&clearInterval(i)},200)};
+    
+
+    
+    writing('console.log("feliz dia do programador");');
+    writing2("print(' happy programmer\'s day')");
+    writing3('System.out.println("bonne fête du programmeur");');
+   
+   
+//]]>
+//]]>
